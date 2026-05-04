6cf5bb10eb89363cd5c5f5189dfe6a0263c8ae89
Adam <adammertz@gmail.com>
2026-05-04 16:01:53 -0700

    Add in initial email, add signal blocking, add a bunch of comments about remaining steps

--- cmd/temporal/workflows/workflow.go
+++ cmd/temporal/workflows/workflow.go
@@ -9,6 +9,8 @@ import (
 	"go.temporal.io/sdk/workflow"
 )
 
+const ApprovedSignal = "approved"
+
 func RegisterLicensePlateWorkflow(ctx workflow.Context, caseId string) error {
 	var caseRecord *models.Case
 	err := workflow.ExecuteActivity(
@@ -19,6 +21,18 @@ func RegisterLicensePlateWorkflow(ctx workflow.Context, caseId string) error {
 
 	if err != nil {
 		log.Println("Failed to get case from database", err)
+		return err
+	}
+
+	err = workflow.ExecuteActivity(
+		ctx,
+		activities.SendEmail,
+		caseRecord.Email,
+		"Your case has been submitted",
+		"We have received your information and will be validating the information and will get back to you with further steps.",
+	).Get(ctx, nil)
+	if err != nil {
+		fmt.Println("errored on sending initial email")
 		return err
 	}
 
@@ -33,6 +47,13 @@ func RegisterLicensePlateWorkflow(ctx workflow.Context, caseId string) error {
 		return err
 	}
 
+	// Block until internal user verifies it's fine
+	workflow.GetSignalChannel(ctx, ApprovedSignal).Receive(ctx, nil)
+	// If not fine do remediation workflow
+
+	// Send email notifying user to schedule an appointment
+
+	// Send email appointment confirmation
 	err = workflow.ExecuteActivity(
 		ctx,
 		activities.SendEmail,
@@ -44,6 +65,16 @@ func RegisterLicensePlateWorkflow(ctx workflow.Context, caseId string) error {
 		log.Println("Failed to send email", err)
 		return err
 	}
+
+	// employee validates gov issued ID and registration
+	// ID & registration uploaded
+	// employee accepts payment
+	// mfg order created
+	// mfg started
+	// mfg completed
+	// shipping started
+	// plate shipped
+	// done
 
 	// TODO: wait for approval
 	return nil
